0666fc239baf0b211f6b1f458766c51f995f0b9a
Web Dev Simplified <webdevsimplified@gmail.com>
2019-09-18 18:21:44 -0500

    Finish Video

--- server.js
+++ server.js
@@ -18,30 +18,20 @@ const posts = [
 ]
 
 app.get('/posts', authenticateToken, (req, res) => {
-  res.json(posts.filter(p => p.username === req.user.name))
-})
-
-app.post('/login', (req, res) => {
-  // Authenticate User
-  const user = { name: req.body.username }
-  const token = jwt.sign(
-    user,
-    process.env.TOKEN_SECRET,
-    { expiresIn: '10m' }
-  )
-  res.json({ token: token })
+  res.json(posts.filter(post => post.username === req.user.name))
 })
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(" ")[1]
+  const token = authHeader && authHeader.split(' ')[1]
   if (token == null) return res.sendStatus(401)
 
-  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err)
     if (err) return res.sendStatus(403)
     req.user = user
     next()
   })
 }
 
-app.listen(process.env.PORT || 3000)
+app.listen(3000)
